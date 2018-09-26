02a834903f0132437f2b50490e24d943ab90990a
rhangelxs <n71user@gmail.com>
2018-09-27 02:17:01 +0300

    Cleanup

--- django_rest_demo/checkin/serializers.py
+++ django_rest_demo/checkin/serializers.py
@@ -33,6 +33,7 @@ class LocationRatioSerializer(serializers.ModelSerializer):
         model = Location
         fields = ("visits", "count", "avg")
         # read_only_fields = ('date',)
+
     visits = serializers.SerializerMethodField()
     count = serializers.SerializerMethodField()
     avg = serializers.SerializerMethodField()
@@ -45,42 +46,3 @@ class LocationRatioSerializer(serializers.ModelSerializer):
 
     def get_visits(self, obj):
         return LocationVisitSerializer(obj.visit_set.all().order_by('id'), many=True, read_only=True).data
-# class RegisterSerializer(serializers.Serializer):
-#     username = serializers.CharField(
-#         max_length=250,
-#         #min_length=8,
-#         validators=[UniqueValidator(queryset=User.objects.all())],
-#         required=True
-#     )
-#     email = serializers.EmailField(required=True,
-#             validators=[UniqueValidator(queryset=User.objects.all())])
-#     password1 = serializers.CharField(write_only=True)
-#     password2 = serializers.CharField(write_only=True)
-#
-#     # We can add custom field, but better ro split it to other url
-#     # gender = serializers.CharField(write_only=True)
-#
-#     def validate(self, data):
-#         if data['password1'] != data['password2']:
-#             raise serializers.ValidationError(_("The two password fields didn't match."))
-#         return data
-#
-#     def custom_signup(self, request, user):
-#         pass
-#
-#     def get_cleaned_data(self):
-#         return {
-#             'username': self.validated_data.get('username', ''),
-#             'password': self.validated_data.get('password1', ''),
-#             'email': self.validated_data.get('email', '')
-#         }
-#
-#     def save(self, request):
-#         # Remove allauth
-#         self.cleaned_data = self.get_cleaned_data()
-#         user = User.objects.create_user(
-#             self.cleaned_data['username'],
-#             self.cleaned_data['email'],
-#             self.cleaned_data['password']
-#         )
-#         return user
