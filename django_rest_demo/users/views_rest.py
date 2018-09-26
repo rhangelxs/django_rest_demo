abcdcfd4eda6dd44084e63140e5c9ac9a78a75f2
rhangelxs <n71user@gmail.com>
2018-09-26 23:16:49 +0300

    Users permissions

--- django_rest_demo/users/views_rest.py
+++ django_rest_demo/users/views_rest.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions, mixins
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly, IsAuthenticatedOrReadOnly
 
 from .serializers import UsersDetailsSerializer
 
@@ -6,6 +7,18 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class UserList(viewsets.ModelViewSet):
+class UserIsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.id == request.user.id
+
+
+class UserList(mixins.RetrieveModelMixin,
+               mixins.UpdateModelMixin,
+               mixins.DestroyModelMixin,
+               mixins.ListModelMixin,
+               viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UsersDetailsSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, UserIsOwnerOrReadOnly]
