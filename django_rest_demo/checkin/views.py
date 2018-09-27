93ed147c919de5f281670076f8fa41016ac8091a
rhangelxs <n71user@gmail.com>
2018-09-27 04:21:10 +0300

    Fix permissions

--- django_rest_demo/checkin/views.py
+++ django_rest_demo/checkin/views.py
@@ -11,11 +11,11 @@ User = get_user_model()
 from .models import Location, Visit
 
 
-# class UserIsOwnerOrReadOnly(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         return obj.id == request.user.id
+class VisitorIsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_id == request.user
 
 
 class LocationList(mixins.CreateModelMixin,
@@ -26,13 +26,14 @@ class LocationList(mixins.CreateModelMixin,
     viewsets.GenericViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationsDetailsSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly, UserIsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class LocationVisit(generics.CreateAPIView):
     def get_queryset(self):
         queryset = Visit.objects.filter(location_id=self.kwargs["pk"])
         return queryset
     serializer_class = LocationVisitSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class LocationRatio(generics.RetrieveAPIView):
     def get_queryset(self):
